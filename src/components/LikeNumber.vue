019ad8b5d478bc1cab7005387369bbc037614f28
rikiwtnb <rikiwtnb@gmail.com>
2020-02-29 22:18:37 +0900

    commit

---
+++ src/components/LikeNumber.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <p>いいね({{ halfNumber }})</p>
+    <button @click="increment">+1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    totalNumber : {
+      type: Number,
+      default: 8       
+    },
+    testProps: {
+      type: String
+    }
+  },
+  computed: {
+    halfNumber() {
+      return this.totalNumber / 2
+    }
+  },
+  methods: {
+    increment: function() {
+      this.number++;
+    }
+  }
+};
+</script>
+
+<style scoped>
+  div {
+    border: 1px solid red;
+  }
+</style>
+
+
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import LikeNumer from './components/LikeNumber.vue'
 
 Vue.config.productionTip = false
+
+//componentのグローバル登録
+Vue.component('LikeNumber', LikeNumer)
 
 new Vue({
   render: h => h(App),
