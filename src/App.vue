2d9f9912c97242fb50e21eab8f6c68c9da70194e
rikiwtnb <rikiwtnb@gmail.com>
2020-03-06 19:35:59 +0900

    add

--- src/App.vue
+++ src/App.vue
@@ -11,9 +11,8 @@
     </keep-alive>
     <div>
       <h2>イベントのフォーム</h2>
-      <label for="title">title</label>
-      <input id="title" type="text" v-model.lazy="eventData.title">
-      <pre>{{ eventData.title }}</pre>
+      <EventTitle v-model="eventData.title"></EventTitle>
+      <EventTitle :value="eventData.title" @input="eventData.title = $event"></EventTitle>
       <label for="maxNumber">最大人数</label>
       <input id="maxNumber" type="number" v-model.number="eventData.maxNumber">
       <p>{{ eventData.maxNumber }}</p>
@@ -35,8 +34,15 @@
       <label for="30">30代</label>
       <p>{{ eventData.target }}</p>
       <p>参加費</p>
-      <input type="text">
-      
+      <input type="radio" id="free" value="無料" v-model="eventData.price">
+      <label for="free">無料</label>
+      <input type="radio" id="paid" value="有料" v-model="eventData.price">
+      <label for="paid">有料</label>
+      <p>開催場所</p>
+      <select v-model="eventData.location">
+        <option v-for="location in locations" :key="location">{{ location }}</option>
+      </select>
+      <p>{{ eventData.location }}</p>
     </div>
   </div>
 </template>
@@ -45,26 +51,31 @@
 import LikeHeader from "./components/LikeHeader.vue";
 import About from "./components/About.vue";
 import Home from "./components/Home.vue";
+import EventTitle from "./components/EventTitle.vue";
 //componentのローカル登録
 export default {
   data() {
     return {
       number: 10,
       currentComponent: "Home",
+      locations: ["東京", "大阪", "名古屋"],
       eventData: {
         title: "タイトル",
         maxNumber: 0,
         host: "",
         detail: "",
         isPrivate: false,
-        target: []
+        target: [],
+        price: "無料",
+        location: "東京"
       }
     };
   },
   components: {
     LikeHeader,
     About,
-    Home
+    Home,
+    EventTitle
   }
 };
 </script>
